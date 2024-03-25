effb8b8c562ae62d4e43f598bfe803824dc9e5c6
Roland Halász <roland.halasz@outlook.com>
2024-03-25 21:18:10 +0100

    feat: install material and bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatGridList } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggle, MatGridList],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

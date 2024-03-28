91d12f8087c9e15e825d38800fa027080b896038
Roland Halász <roland.halasz@outlook.com>
2024-03-28 11:11:34 +0100

    feat: compare mat2 and mat3 themes

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -6,16 +6,17 @@ import {
   MatCardActions,
   MatCardContent
 } from '@angular/material/card';
-import { MatButton } from '@angular/material/button';
+import { MatAnchor, MatButton } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatToolbar } from '@angular/material/toolbar';
-import { MatSlider, MatSliderThumb } from '@angular/material/slider';
+import { MatSlider } from '@angular/material/slider';
 import { MatOption } from '@angular/material/autocomplete';
 import { MatFormField } from '@angular/material/form-field';
 import { MatSelect } from '@angular/material/select';
 import { MatRadioButton, MatRadioGroup } from '@angular/material/radio';
 import { MatCheckbox } from '@angular/material/checkbox';
 import { MatInput } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
@@ -35,7 +36,9 @@ import { MatInput } from '@angular/material/input';
     MatRadioButton,
     MatRadioGroup,
     MatCheckbox,
-    MatInput
+    MatInput,
+    MatAnchor,
+    MatIcon
   ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
